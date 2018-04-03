c9b71099652271ae52e926904c90ac2f5c7f6105
kikuri <blueending3@gmail.com>
2018-04-03 18:30:43 +0900

    add ARPhotoFunction

--- magicpop-travel-mvp/ViewController.swift
+++ magicpop-travel-mvp/ViewController.swift
@@ -10,71 +10,84 @@ import UIKit
 import SceneKit
 import ARKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
-
+class ViewController: UIViewController {
+    
     @IBOutlet var sceneView: ARSCNView!
+    var Button: UIButton?
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
         let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
         sceneView.session.run(configuration)
+        
+        let screenWidth:CGFloat = self.view.frame.width
+        let screenHeight:CGFloat = self.view.frame.height
+        
+        Button = UIButton(type: .custom)
+        Button?.setTitle("Tap me!", for:UIControlState.normal)
+        Button?.frame = CGRect(x:screenWidth/4, y:screenHeight/2,width:screenWidth/2, height:50)
+        Button?.translatesAutoresizingMaskIntoConstraints = false
+        Button?.layer.cornerRadius = 10.0
+        Button?.addTarget(self, action: #selector(self.photoButtonTapped(_:)), for: .touchUpInside)
+        
+        self.view.addSubview(Button!)
+
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
         
-        // Pause the view's session
         sceneView.session.pause()
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Release any cached data, images, etc that aren't in use.
+    @IBAction func photoButtonTapped(_ sender: Any) {
+        showUIImagePicker()
     }
-
-    // MARK: - ARSCNViewDelegate
     
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+    private func showUIImagePicker() {
+        if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
+            let pickerView = UIImagePickerController()
+            pickerView.sourceType = .photoLibrary
+            pickerView.delegate = self
+            pickerView.modalPresentationStyle = .overFullScreen
+            self.present(pickerView, animated: true, completion: nil)
+        }
+    }
+    
+    private func setImageToScene(image: UIImage) {
+        if let camera = sceneView.pointOfView {
+            let position = SCNVector3(x: 0, y: 0, z: -0.5) // 偏差のベクトルを生成する
+            let convertPosition = camera.convertPosition(position, to: nil)
+            let node = createPhotoNode(image, position: convertPosition)
+            self.sceneView.scene.rootNode.addChildNode(node)
+        }
+    }
+    
+    private func createPhotoNode(_ image: UIImage, position: SCNVector3) -> SCNNode {
         let node = SCNNode()
-     
+        let scale: CGFloat = 0.3
+        let geometry = SCNBox(width: image.size.width * scale / image.size.height,
+                              height: scale,
+                              length: 0.00000001,
+                              chamferRadius: 0.0)
+        geometry.firstMaterial?.diffuse.contents = image
+        node.geometry = geometry
+        node.position = position
         return node
     }
-*/
+}
+
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
-        
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            setImageToScene(image: image)
+        }
+        picker.dismiss(animated: true, completion: nil)
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion: nil)
     }
 }
